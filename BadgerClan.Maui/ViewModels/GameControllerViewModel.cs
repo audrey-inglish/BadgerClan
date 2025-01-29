b8f6e14e0116fbc56b074f85c4d90f3346d85454
Audrey Inglish <audreyinglish@gmail.com>
2025-01-29 12:39:37 -0700

    UI progress - dropdown and button

--- BadgerClan.Maui/ViewModels/GameControllerViewModel.cs
+++ BadgerClan.Maui/ViewModels/GameControllerViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BadgerClan.Maui.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,31 @@ using System.Threading.Tasks;
 
 namespace BadgerClan.Maui.ViewModels;
 
-public class GameControllerViewModel
+public partial class GameControllerViewModel : ObservableObject
 {
+    private readonly IApiService apiService;
 
+    [ObservableProperty]
+    private string currentStrategy;
+
+    public List<string> AvailableStrategies { get; } = new List<string>
+    { "RunGunStrategy", "DoNothingStrategy" }; // should I use "Strategy" suffix?
+
+
+    public GameControllerViewModel(IApiService apiService)
+    {
+        this.apiService = apiService;
+        CurrentStrategy = AvailableStrategies[0];
+    }
+
+
+    [RelayCommand]
+    private async Task SetStrategy(string strategyChoice)
+    {
+        await apiService.SetStrategyAsync(strategyChoice);
+
+        // the UI will update automatically
+        CurrentStrategy = strategyChoice;
+
+    }
 }
