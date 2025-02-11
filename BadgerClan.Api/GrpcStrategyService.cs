622e82cc37a0a1a9e58dfc00a3595b1ca45fc6f6
Audrey Inglish <audreyinglish@gmail.com>
2025-02-10 20:25:17 -0700

    forgot to...you know...set the strategy in SetStrategy

--- BadgerClan.Api/GrpcStrategyService.cs
+++ BadgerClan.Api/GrpcStrategyService.cs
@@ -3,10 +3,11 @@ using ProtoBuf.Grpc.Server;
 
 namespace BadgerClan.Api;
 
-public class GrpcStrategyService : IStrategyService
+public class GrpcStrategyService(StrategyService service) : IStrategyService
 {
     public Task SetStrategy(SetStrategyRequest request)
     {
+        service.SetStrategy(request.StrategyName);
         return Task.FromResult(new SetStrategyResponse
         {
             IsSuccess = true,
