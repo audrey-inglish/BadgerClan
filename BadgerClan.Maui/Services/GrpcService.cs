5bd8998e805dcc5dba18411f7bff713bc935f925
Audrey Inglish <audreyinglish@gmail.com>
2025-02-10 22:33:58 -0700

    before tweaking viewmodel

---
+++ BadgerClan.Maui/Services/GrpcService.cs
@@ -0,0 +1,17 @@
+﻿//using BadgerClan.Shared;
+//using Grpc.Net.Client;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace BadgerClan.Maui.Services;
+
+//public class GrpcService : IGrpcService
+//{
+//    public Task SetStrategyAsync(string apiUrl, string strategy)
+//    {
+       
+//    }
+//}
